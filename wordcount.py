b7a2bb8ff803fc12a426743686907ac4b3f0e003
Matthew Glassman <matthewglassman78@gmail.com>
2023-11-11 10:18:23 -0500

    Added prompt for user input. Print results out to the screen.

--- wordcount.py
+++ wordcount.py
@@ -1,4 +1,7 @@
-getstring = input()
+getstring = input("Please type in some words and I will tell you how many you typed.   ")
 
 #The number of words can be found by counting the number of spaces and adding 1 more than that number. 
-totalnumofword = getstring.count(" ") + 1
+totalnumofwords = getstring.count(" ") + 1
+
+#Print the total to the screen
+print(totalnumofwords)
