074f32f5836f0046c17da960b9f37c666ba23588
Matthew Glassman <matthewglassman78@gmail.com>
2023-11-12 20:24:03 -0500

    Simple program that takes integer input and then creates a letter that many times

---
+++ lettermultiplier.py
@@ -0,0 +1,8 @@
+#Create a program that takes an integer as an input and creates that number of o's in the word 'spooky'.
+
+number_of_o = int(input())
+
+o = "o" * number_of_o
+
+print ("sp" + o +"ky")
+
