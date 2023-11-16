ef4a9b341c18ed5a8fc9b7dc29c8b4d31206cda4
Matthew Glassman <matthewglassman78@gmail.com>
2023-11-16 18:57:08 -0500

    Creating variables to hold input for the last 4 digits of a phone number.

---
+++ should_I_answer.py
@@ -0,0 +1,7 @@
+#A script that checks to see if a pattern indicates a telemarketer is calling or not and whether to answer the call accordingly.
+
+first_digit = int(input())
+second_digit = int(input())
+thrid_digit = int(input())
+fourth_digit = int(input())
+
