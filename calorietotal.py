88d0e5db11fbaf5c676de9a78c8406d0e021dc6c
Matthew Glassman <matthewglassman78@gmail.com>
2023-11-18 12:58:49 -0500

    Added conditional statement to find burger calorie amount.

---
+++ calorietotal.py
@@ -0,0 +1,23 @@
+#A problem using conditional statements to take an order and sum the calories of the items.
+
+#Gather user item order and set to a variable
+
+burger_choice = int(input())
+sides_choice = int(input())
+drink_choice = int(input())
+dessert_choice = int(input
+
+burger_calories = 0
+sides_calories = 0
+drink_calories = 0
+dessert_calories = 0
+
+if burger_choice == 1:
+    burger_calories = 461
+ elif burger_choice == 2:
+    burger_calories = 431
+elif burger_choice == 3:
+    burger_calories == 420
+else
+    burger_calories == 0
+    
