9eb05e43b74aea5ee78afde2092595f4fa23c37d
Matthew Glassman <matthewglassman78@gmail.com>
2023-11-18 08:45:36 -0500

    Created conditional logic to check numbers to figure out if caller is telemarketer or not.

--- should_I_answer.py
+++ should_I_answer.py
@@ -2,6 +2,13 @@
 
 first_digit = int(input())
 second_digit = int(input())
-thrid_digit = int(input())
+third_digit = int(input())
 fourth_digit = int(input())
 
+if ((first_digit == 8 or first_digit == 9) and
+    (second_digit == third_digit) and 
+    (fourth_digit == 8 or fourth_digit == 9)):
+    print("Ignore")
+else:
+    print("Answer")
+ 
