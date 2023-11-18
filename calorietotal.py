cf6bddbcc79f8c411d7eaa2a120fdfc65c1fd24d
Matthew Glassman <matthewglassman78@gmail.com>
2023-11-18 13:30:52 -0500

    Ran some tests due to logic error.  Added code to test addition.

--- calorietotal.py
+++ calorietotal.py
@@ -4,30 +4,35 @@
 
 burger_choice = int(input())
 sides_choice = int(input())
-drink_choice = int(input())
-dessert_choice = int(input
+#drink_choice = int(input())
+#dessert_choice = int(input())
 
 burger_calories = 0
 sides_calories = 0
-drink_calories = 0
-dessert_calories = 0
+#drink_calories = 0
+#dessert_calories = 0
 
 #Get burger_calories
 if burger_choice == 1:
     burger_calories = 461
- elif burger_choice == 2:
+elif burger_choice == 2:
     burger_calories = 431
 elif burger_choice == 3:
-    burger_calories == 420
-else
-    burger_calories == 0
-    
+    burger_calories = 420
+elif burger_choice == 4:
+    burger_calories = 0
+
 #Get sides_calories
 if sides_choice== 1:
     sides_calories = 100
- elif sides_choice == 2:
+elif sides_choice == 2:
     sides_calories = 57
 elif sides_choice == 3:
-    sides_calories == 70
-else
-    sides_calories == 0
+    sides_calories = 70
+elif sides_choice == 4:
+    sides_calories = 0
+
+
+total_calories = burger_calories + sides_calories
+
+print(total_calories)
