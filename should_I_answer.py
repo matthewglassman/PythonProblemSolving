0ee6e2df7daf2539a103fc041cfc07a35732daf8
Matthew Glassman <matthewglassman78@gmail.com>
2023-11-18 08:50:37 -0500

    Slightly updated indentation for better reading.

--- should_I_answer.py
+++ should_I_answer.py
@@ -6,8 +6,8 @@ third_digit = int(input())
 fourth_digit = int(input())
 
 if ((first_digit == 8 or first_digit == 9) and
-    (second_digit == third_digit) and 
-    (fourth_digit == 8 or fourth_digit == 9)):
+        (second_digit == third_digit) and 
+        fourth_digit == 8 or fourth_digit == 9)):
     print("Ignore")
 else:
     print("Answer")
