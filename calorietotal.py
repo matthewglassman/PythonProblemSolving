c94f67116dff3937b38e9a8f5cd20b92d22247fb
Matthew Glassman <matthewglassman78@gmail.com>
2023-11-18 13:37:43 -0500

    Added conditional choices to determine drink calories.

--- calorietotal.py
+++ calorietotal.py
@@ -4,13 +4,13 @@
 
 burger_choice = int(input())
 sides_choice = int(input())
-#drink_choice = int(input())
-#dessert_choice = int(input())
+drink_choice = int(input())
+dessert_choice = int(input())
 
 burger_calories = 0
 sides_calories = 0
-#drink_calories = 0
-#dessert_calories = 0
+drink_calories = 0
+dessert_calories = 0
 
 #Get burger_calories
 if burger_choice == 1:
@@ -19,7 +19,7 @@ elif burger_choice == 2:
     burger_calories = 431
 elif burger_choice == 3:
     burger_calories = 420
-elif burger_choice == 4:
+else:
     burger_calories = 0
 
 #Get sides_calories
@@ -29,10 +29,19 @@ elif sides_choice == 2:
     sides_calories = 57
 elif sides_choice == 3:
     sides_calories = 70
-elif sides_choice == 4:
+else:
     sides_calories = 0
 
+#Get drink_calories
+if drink_choice == 1:
+    drink_calories = 130
+elif drink_choice == 2:
+    drink_calories = 160
+elif drink_choice == 3:
+    drink_calories = 118
+else:
+    drink_calories = 0
 
-total_calories = burger_calories + sides_calories
+total_calories = burger_calories + sides_calories + drink_calories
 
 print(total_calories)
