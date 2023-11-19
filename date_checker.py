90c5603c9f23357ade9b0705ca6d51afedb2ca5b
Matthew Glassman <matthewglassman78@gmail.com>
2023-11-18 20:57:44 -0500

    Had to change 30 to 31 for the days.

--- date_checker.py
+++ date_checker.py
@@ -4,7 +4,7 @@
 month = int(input())
 day = int(input())
 
-if ((month < 2) and (day <= 30)) or ((month == 2) and (day <= 17)):
+if ((month < 2) and (day <= 31) or ((month == 2) and (day <= 17)):
     print("Before")
 elif ((month >= 2) and (day >= 19)) or (month > 2) :
     print("After")
