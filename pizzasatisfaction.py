ad5baf8ee24bf050e371aedacb0a1c30084f9520
Matthew Glassman <matthewglassman78@gmail.com>
2023-11-19 10:57:09 -0500

    Corrected spelling of fairly.

--- pizzasatisfaction.py
+++ pizzasatisfaction.py
@@ -12,6 +12,6 @@ cheesiness =  int(input())
 if ((pizza_width == 3 and cheesiness >= 95)):
     print("C.C. is absolutely satisfied with her pizza.")
 elif ((pizza_width == 1 and cheesiness <= 50)):
-    print("C.C. is failry satisfied with her pizza.")
+    print("C.C. is fairly satisfied with her pizza.")
 else:
     print("C.C. is very satisfied with her pizza.")
