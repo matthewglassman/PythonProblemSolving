ee57eae728fab304ae4289c0e906d2a1a8ca606b
Matthew Glassman <matthewglassman78@gmail.com>
2023-11-19 10:44:53 -0500

    Added comments to describe what script will do.

---
+++ pizzasatisfaction.py
@@ -0,0 +1,9 @@
+#This little script takes two integer inputs and determines the level of pizza satisfaction for the user based on the following criteria:
+
+# User will be absolutely satisfied if the pizza she gets has a width of 
+ # units and an extra-cheesiness of at least 
+# User will be fairly satisfied if the pizza she gets has a width of 
+ # unit and an extra-cheesiness of at most 
+# User will be very satisfied with any other pizza she receives.
+
+
