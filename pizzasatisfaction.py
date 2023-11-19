b8ffac2707228bb8306be4c7e81a9448112b89bc
Matthew Glassman <matthewglassman78@gmail.com>
2023-11-19 10:55:47 -0500

    Edited print statement to replace underscore with parentheses.

--- pizzasatisfaction.py
+++ pizzasatisfaction.py
@@ -14,4 +14,4 @@ if ((pizza_width == 3 and cheesiness >= 95)):
 elif ((pizza_width == 1 and cheesiness <= 50)):
     print("C.C. is failry satisfied with her pizza.")
 else:
-    print("C.C. is very satisfied with her pizza."_
+    print("C.C. is very satisfied with her pizza.")
