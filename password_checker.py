2d69d7472d9858b9e50967fbd902c2c4a3bfc849
Matthew Glassman <matthewglassman78@gmail.com>
2023-11-25 12:33:19 -0500

    Laying out what the script should do and taking password as input.

---
+++ password_checker.py
@@ -0,0 +1,9 @@
+# A script to check if a password is valid based on the following rules:
+# 1. Must be a string between 8 and 12 characters (inclusive) long.
+# 2. Must contain at least 3 lowercase letters.
+# 3. Must contain at least 2 uppercase letters.
+# 4. Must contain at least 1 digit.
+
+#Taking input from user as the password to check for validity.
+
+password = str(input())
