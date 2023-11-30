4bed73d315213613d66f4f0e822f5ab32e16fbcb
Matthew Glassman <matthewglassman78@gmail.com>
2023-11-30 11:23:55 -0500

    Added logic to total uppercase, lowercase and digits in password.

--- password_checker.py
+++ password_checker.py
@@ -8,9 +8,21 @@
 
 password = str(input()) 
  
-for characters in password:
-    if (len(password)  >= 8) and (len(password) <= 12):
-        print('Good')
-    else:
-        print('Bad')
-   
+count_lower = 0
+count_upper = 0
+count_digit = 0
+ 
+ 
+if (len(password)  >= 8) and (len(password) <= 12):
+    for characters in password:
+        if characters >= 'A' and characters <= 'Z':
+            count_upper = count_upper + 1
+        elif characters >= 'a' and characters <= 'z':
+            count_lower = count_lower + 1
+         elif characters >= '0' and characters <= '9':
+            count_digit = count_digit + 1
+else:
+    print("Password is not long enough")
+  
+# print(count_upper)
+# print(count_lower)   
